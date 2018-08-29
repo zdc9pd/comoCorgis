9dcda5e4872abdb89c9bd70d33843a822b094e5d
Zac <biogenetic40379@gmail.com>
2018-08-28 20:45:15 -0500

    Added in javascript for kids and other pets

--- public/corgiScripts.js
+++ public/corgiScripts.js
@@ -1,3 +1,27 @@
 function puppySubmit() {
     console.log("Clicking submit!");
-}
+};
+
+function petSelection() {
+    var selection = document.querySelector('input[name = "petRadio"]:checked').value;
+    if(selection == "yesPet") {
+        console.log(selection);
+        document.getElementById("petInput").style.display='block';
+    }
+    else {
+        document.getElementById("petInput").style.display='none';
+        console.log(selection);
+    }
+};
+
+function kidSelection() {
+    var selection = document.querySelector('input[name = "kidRadio"]:checked').value;
+    if(selection == "yesKids") {
+        console.log(selection);
+        document.getElementById("kidInput").style.display='block';
+    }
+    else {
+        document.getElementById("kidInput").style.display='none';
+        console.log(selection);
+    }
+};
